dcfc2b6de25742d4bb0f413062486de25a85315f
Aaron Sun <axiverse@gmail.com>
2020-02-27 01:08:37 -0800

    Add an initial example.

---
+++ Examples/NestedCommands/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+using CommandLineInterface;
+
+namespace NestedCommands
+{
+    class Program
+    {
+        [Command(nameof(Branch))]
+        [Description("en-US", "Hello world")]
+        public Branch Branch;
+
+        [Command(nameof(Commit))]
+        public Commit Commit;
+
+        [Parameter("v", "version")]
+        public string Version;
+
+        static void Main(string[] args)
+        {
+            var program = ArgumentParser.Parse<Program>(args);
+            if (!string.IsNullOrEmpty(program.Version))
+            {
+                Console.WriteLine(Assembly.GetExecutingAssembly().GetName().Version);
+            }
+        }
+    }
+}
